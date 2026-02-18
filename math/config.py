5a90ef8b7177dfb36c1ff43d705d80bc223bd02c
Rakshit Hooda <rakshit.official703@gmail.com>
2026-02-18 22:04:24 +0530

    feat: add initial game configurations

---
+++ math/config.py
@@ -0,0 +1,18 @@
+# GAME CONFIGS
+
+# Grid settings
+NUM_REELS = 5
+NUM_ROWS = 3
+
+# Payline settings
+NUM_PAYLINES = 20
+
+# Betting settings
+LINE_BET = 1.0        # For simulation simplicity
+TOTAL_BET = LINE_BET * NUM_PAYLINES
+
+# Simulation settings
+DEFAULT_SPINS = 1_000_000
+
+# Debug
+DEBUG_MODE = False
